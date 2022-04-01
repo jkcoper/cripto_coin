b37d33eb4e3e5176e3aa72945c4f6e10dd84e24d
jkcoper <jcoper@gmail.com>
2022-03-31 22:06:03 -0500

    primer commit

---
+++ archivo.js
@@ -0,0 +1,9 @@
+let superheroes =[
+    "Batman",
+    "Mujer maravilla",
+    "Deadpool"
+ ];
+
+/*  console.log(superheroes); */
+
+module.exports = superheroes;
---
+++ archivo2.js
@@ -0,0 +1,4 @@
+let heroes = require("./archivo.js");
+/* console.log(heroes); */
+var moment = require("moment");
+console.log(moment().format("MM DD YY"));
